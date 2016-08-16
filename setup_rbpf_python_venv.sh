4494e4c85a822ba8115685cc0a523672ef4a0134
Jonathan Kuck <jdkuck@gmail.com>
2016-08-15 23:02:45 -0700

    script for submitting multiple SCAIL jobs

--- setup_rbpf_python_venv.sh
+++ setup_rbpf_python_venv.sh
@@ -9,4 +9,4 @@ pip install filterpy
 pip install munkres
 
 cd /afs/cs.stanford.edu/u/jkuck/rotation3/rbpf_target_tracking
-python rbpf_KITTI_det_scores.py 100
+python rbpf_KITTI_det_scores.py $num_particles
