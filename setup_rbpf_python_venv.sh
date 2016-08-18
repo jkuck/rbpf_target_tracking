bfa30047d41a192828320d5307dc325ffef0db16
Jonathan Kuck <jdkuck@gmail.com>
2016-08-18 11:29:11 -0700

    Fixed bash execution scripts for Atlas
    Removed ml load python (from Sherlock).
    Updated Atlas run_experiment_batch.sh for exucuting runs in parallel.

--- setup_rbpf_python_venv.sh
+++ setup_rbpf_python_venv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ml load python/2.7.5
-
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
@@ -11,4 +9,4 @@ pip install filterpy
 pip install munkres
 
 cd /afs/cs.stanford.edu/u/jkuck/rotation3/rbpf_target_tracking
-python rbpf_KITTI_det_scores.py $num_particles $include_ignored_gt $include_dontcare_in_gt $use_regionlets_and_lsvm $sort_dets_on_intervals
+python rbpf_KITTI_det_scores.py $num_particles $include_ignored_gt $include_dontcare_in_gt $use_regionlets_and_lsvm $sort_dets_on_intervals $RUN_IDX
