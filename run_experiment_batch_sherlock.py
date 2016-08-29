bd4fa96633f080a6dc219cef5b3be3ebcc588980
Jonathan David Kuck <kuck@sherlock-ln04.stanford.edu>
2016-08-28 18:52:32 -0700

    Experiment run here (Sherlock):
    debugged_death_probs_3TimeStepsMarkovChain

--- run_experiment_batch_sherlock.py
+++ run_experiment_batch_sherlock.py
@@ -10,7 +10,7 @@ SEQUENCES_TO_PROCESS = [i for i in range(21)]
 NUM_PARTICLES_TO_TEST = [100]
 DIRECTORY_OF_ALL_RESULTS = '/scratch/users/kuck/rbpf_results'
 
-CUR_EXPERIMENT_BATCH_NAME = 'debugged_death_probs2'
+CUR_EXPERIMENT_BATCH_NAME = 'debugged_death_probs_3TimeStepsMarkovChain'
 RUN_EVALUATION = True
 
 
