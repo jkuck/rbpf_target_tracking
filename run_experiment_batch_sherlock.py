2818e44aa60ed95aeba7769e7283ee1690b3978a
Jonathan David Kuck <kuck@sherlock-ln04.stanford.edu>
2016-08-30 11:53:15 -0700

    Experiment run here (Sherlock):
    debugged_death_probs_3TimeStepsMarkovChain_onlineResults_goodIDS_variable_R

--- run_experiment_batch_sherlock.py
+++ run_experiment_batch_sherlock.py
@@ -7,10 +7,10 @@ SEQUENCES_TO_PROCESS = [i for i in range(21)]
 #SEQUENCES_TO_PROCESS = [11]
 #SEQUENCES_TO_PROCESS = [0]
 #NUM_PARTICLES_TO_TEST = [25, 100]
-NUM_PARTICLES_TO_TEST = [100, 400]
+NUM_PARTICLES_TO_TEST = [100]
 DIRECTORY_OF_ALL_RESULTS = '/scratch/users/kuck/rbpf_results'
 
-CUR_EXPERIMENT_BATCH_NAME = 'debugged_death_probs_3TimeStepsMarkovChain_onlineResults_goodIDS'
+CUR_EXPERIMENT_BATCH_NAME = 'debugged_death_probs_3TimeStepsMarkovChain_onlineResults_goodIDS_variable_R'
 RUN_EVALUATION = True
 
 
